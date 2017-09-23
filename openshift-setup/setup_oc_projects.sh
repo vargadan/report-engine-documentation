2a9231aafa16d8b140f15ca5d533c61a2b948885
vargadan <danielvrg@gmail.com>
2017-09-23 20:39:00 +0200

    config changes

--- openshift-setup/setup_oc_projects.sh
+++ openshift-setup/setup_oc_projects.sh
@@ -4,10 +4,26 @@ oc new-project reportengine-dev --display-name="Report Engine - DEV"
 oc new-project reportengine-it --display-name="Report Engine - IT"
 oc new-project reportengine-cicd --display-name="Report Engine - CICD"
 
-oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:default -n reportengine-dev
 oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:default -n reportengine-cicd
-oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:jenkins -n reportengine-dev
 oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:jenkins -n reportengine-cicd
-oc process -f jenkins.yaml | oc create -f -
+
+oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:default -n reportengine-dev
+oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:jenkins -n reportengine-dev
+
+oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:default -n reportengine-it
+oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:jenkins -n reportengine-it
+
+
+
 oc process -f nexus.yaml | oc create -f -
 oc process -f sonarqube.yaml | oc create -f -
+
+oc project reportengine-dev
+oc process -f rabbitmq.yaml | oc create -f -
+
+oc project reportengine-it
+oc process -f rabbitmq.yaml | oc create -f -
+
+oc project reportengine-cicd
+
+oc process -f pipelines.yaml | oc create -f -
