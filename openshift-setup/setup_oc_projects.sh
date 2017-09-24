8f7fc0c19dd7d0361dc875c8ecebf08abaacb8ad
vargadan <danielvrg@gmail.com>
2017-09-24 23:59:55 +0200

    project setup

--- openshift-setup/setup_oc_projects.sh
+++ openshift-setup/setup_oc_projects.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
-oc new-project reportengine-dev --display-name="Report Engine - DEV"
-oc new-project reportengine-it --display-name="Report Engine - IT"
 oc new-project reportengine-cicd --display-name="Report Engine - CICD"
-
 oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:default -n reportengine-cicd
 oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:jenkins -n reportengine-cicd
 
-oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:default -n reportengine-dev
-oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:jenkins -n reportengine-dev
-
-oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:default -n reportengine-it
-oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:jenkins -n reportengine-it
-
-
-
 oc process -f nexus.yaml | oc create -f -
 oc process -f sonarqube.yaml | oc create -f -
+oc process -f pipelines.yaml | oc create -f -
 
-oc project reportengine-dev
+oc new-project reportengine-dev --display-name="Report Engine - DEV"
+oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:default -n reportengine-dev
+oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:jenkins -n reportengine-dev
 oc process -f rabbitmq.yaml | oc create -f -
 
-oc project reportengine-it
+oc new-project reportengine-it --display-name="Report Engine - IT"
+oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:default -n reportengine-it
+oc policy add-role-to-user edit system:serviceaccount:reportengine-cicd:jenkins -n reportengine-it
 oc process -f rabbitmq.yaml | oc create -f -
 
 oc project reportengine-cicd
-
-oc process -f pipelines.yaml | oc create -f -
